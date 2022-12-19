5d0745ca7aca320aa88d5b4a4142628585aae979
root <root@Ty-Laptop.localdomain>
2022-12-19 13:35:57 -0500

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,24 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import {
+   trees as parkTrees,
+   wildlife as parkWildlife,
+  } from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  console.log(parkTrees);
+  parkWildlife();
 
-  return <h1>Colorado State Parks!</h1>;
+  return(
+    <div>
+      <MesaVerde />
+    </div>
+  )
+
 }
+
+
+
+export default ColoradoStateParks;
